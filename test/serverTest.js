9cb67764149e3df3798293258a8a50a890da67fd
Bipin babu <bipin.u.babu@gmail.com>
2017-06-27 09:52:24 -0700

    Added unit test cases for testing server functionality

---
+++ test/serverTest.js
@@ -0,0 +1,33 @@
+var chai = require('chai'),
+    mocha = require('mocha'),
+    should = chai.should();
+var io = require('socket.io-client');
+
+
+describe("socket.io test", function () {
+
+    var server,
+        options = {
+            transports: ['websocket'],
+            'force new connection': true
+        };
+
+    beforeEach(function (done) {
+        // start the server
+        server = require('../server').server;
+        done();
+    });
+
+    it("should be able to send and recieve message", function (done) {
+        var client = io.connect("http://localhost:3000", options);
+        client.once("connect", function () {
+            client.once("receive-message", function (message) {
+                message.should.equal("message1");
+                client.disconnect();
+                done();
+            });
+            client.emit("new-message", "message1");
+        });
+    });
+
+});
